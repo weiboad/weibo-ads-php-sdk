74f585504aaf7e0ae0e7d02b4399b480bc0cae5b
hyq <yongqiang7@staff.weibo.com>
2017-12-26 16:56:38 +0800

    添加数据统计和视频分片

--- src/WeiboAd/Core/InsightApi.php
+++ src/WeiboAd/Core/InsightApi.php
@@ -11,6 +11,7 @@ class InsightApi extends AbstractApi
     const URI_DEMOGRAPHY = "/insights/demography";
     const URI_EFFECT     = "/insights/effect";
     const URI_LAYER      = "/insights/layer";
+    const URI_GRAPH      = "/insights/graph";
 
     /**
      * @param array $timeInterval
@@ -97,6 +98,36 @@ class InsightApi extends AbstractApi
 
         ];
         $url = self::URI_LAYER . "?data=" . json_encode($params);
+        $data =  $this->api->getApiRequest()->call($url, 'GET');
+        return $data;
+    }
+
+    /**
+     * 获取实时数据
+     * @param array $timeInterval
+     * @param array $field
+     * @param array $dimension
+     * @param array $granularity
+     * @param $param
+     * @param array $orderBy
+     * @param $orderMode
+     * @param $page
+     * @param $rows
+     * @return mixed
+     */
+    public function graph(array $timeInterval, array $field, array $dimension, array $granularity, $param, array $orderBy, $orderMode, $page, $rows) {
+        $params = [
+            'time_interval' => $timeInterval,
+            'order_by' => $orderBy,
+            'granularity' => $granularity,
+            'dimension' => $dimension,
+            'page' => $page,
+            'rows' => $rows,
+            'field' => $field,
+            'param' => $param,
+            'order_mode' => $orderMode
+        ];
+        $url = self::URI_GRAPH . "?data=" . json_encode($params);
         $data =  $this->api->getApiRequest()->call($url, 'GET');
         return $data;
     }
--- src/WeiboAd/Core/VideoApi.php
+++ src/WeiboAd/Core/VideoApi.php
@@ -16,6 +16,8 @@ class VideoApi extends AbstractApi
     const URI_READING = "/video";
     const URI_LIST    = "/video/list";
     const URI_CREATE  = "/video";
+    const URI_INIT  = "/video/init";
+    const URI_SEGMENT  = "/video/segment";
 
 
     /**
@@ -65,5 +67,47 @@ class VideoApi extends AbstractApi
         ];
         $data =  $this->api->getApiRequest()->call(self::URI_CREATE, 'POST', [], $multiData);
         return new Video($data);
+    }
+
+    /**
+     * 视频上传初始化
+     * @param $name
+     * @param $fileMD5
+     * @param $fileSize
+     * @return mixed
+     */
+    public function init($name, $fileMD5, $fileSize) {
+        $multiData = [
+            ['name' => 'video_name', 'contents' => $name],
+            ['name' => 'file_md5', 'contents' => $fileMD5],
+            ['name' => 'file_size', 'contents' => $fileSize],
+        ];
+        $data =  $this->api->getApiRequest()->call(self::URI_INIT, 'POST', [], $multiData);
+        return $data;
+    }
+
+    /**
+     * 视频分片上传
+     * @param $fileToken
+     * @param $fileMD5
+     * @param $segmentSize
+     * @param $length
+     * @param $segmentMD5
+     * @param $index
+     * @param $file
+     * @return mixed
+     */
+    public function segment($fileToken, $fileMD5, $segmentSize, $length, $segmentMD5, $index, $file) {
+        $multiData = [
+            ['name' => 'file_token', 'contents' => $fileToken],
+            ['name' => 'file_md5', 'contents' => $fileMD5],
+            ['name' => 'length', 'contents' => $length],
+            ['name' => 'segment_size', 'contents' => $segmentSize],
+            ['name' => 'segment_md5', 'contents' => $segmentMD5],
+            ['name' => 'index', 'contents' => $index],
+            ['name' => 'file', 'contents' => $file],
+        ];
+        $data =  $this->api->getApiRequest()->call(self::URI_SEGMENT, 'POST', [], $multiData);
+        return $data;
     }
 }
